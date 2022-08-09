ce8be6e36aacc00dfef21bdb4fa5018c46dde9ce
O-RGB <54633251+O-RGB@users.noreply.github.com>
2022-08-09 23:29:59 +0700

    typingblind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'typingblind';
+
+  @HostListener('document:keypress', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) { 
+    if(arrayText[event.key]){
+      this.tts(event.key)
+    }
+  }
+
+  tts(filename:string){
+    var audio = new Audio();
+    audio.src = '../assets/sound/thai-sound/'+filename+'.mp3'
+    audio.load();
+    audio.play();
+  }
+
+  ngOnInit(): void {
+    
+  }
+  
+}
+
+
+const arrayText:any = {
+  "ก":'ก',
+  "ข":'ข',
+  "ฃ":'ฃ',
+  "ค":'ค',
+  "ฅ":'ฅ',
+  "ฆ":'ฆ',
+  "ง":'ง',
 }
